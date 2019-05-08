0f7c3940a0db7ff7c145ca731308ec8c1de2b936
Roy Leibovitz <royleibo212@gmail.com>
2019-05-08 18:40:32 +0300

    creating packages

--- src/Ass5Game.java
+++ src/Ass5Game.java
@@ -1,7 +1,10 @@
+import rungame.Game;
 /**
  * This class runs the game.
+ *
+ * @author Roy Leibovitz
  */
-public class Ass3Game {
+public class Ass5Game {
 
     /**
      * This is the main method which creates an instance of Game and call
@@ -11,6 +14,7 @@ public class Ass3Game {
      */
     public static void main(String[] args) {
         Game g = new Game();
+        g.initialize();
         g.run();
     }
 }
